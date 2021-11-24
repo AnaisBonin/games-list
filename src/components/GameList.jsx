d15d6a32e1f6d512d65e9b9df4abd4f8cee20087
AnaisBonin <anaisboninfr@gmail.com>
2021-11-24 10:48:53 +0100

    filtered Games in GameList to diplay >4,5 rated games only on click

--- src/components/GameList.jsx
+++ src/components/GameList.jsx
@@ -1,26 +1,45 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import Game from './Game';
+import "./GameList.css"
 
 const GameList = () => {
     const [gameList, setGameList] = useState([]);
+    const [showBestGames, setShowBestGames] = useState(false);
 
     useEffect(() =>{
+        getAllGames();
+    }, [])
+
+    const getAllGames = () => {
         axios.get('https://apis.wilders.dev/wild-games/games')
         .then(response => response.data)
         .then(data => setGameList(data))
-    }, [])
+    }
 
-    console.log(gameList);
+    const filterGames = () => {if (showBestGames === false){
+            setShowBestGames(true)
+            setGameList(gameList.filter(game => game.rating >= 4.5))
+        }else{
+            setShowBestGames(false)
+            getAllGames();
+        }};
+    
+
 
     return (
-        <div>
+        <div className="game-list">
+            <button type="button" className="button-filter-games" onClick={filterGames}> 
+            {!showBestGames ? "Best Games ( > 4.5/5)" : "All Games"}
+            </button>
+            <div className="game-cards-container">
             {gameList.map(game => {
                 console.log(game.background_image)
                 return (
                     <Game key={game.id} name={game.name} image={game.background_image} rating={game.rating}/>
                 )
             })}
+            </div>
 
             
         </div>
