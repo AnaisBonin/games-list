60365c955f2caf7af210eccda302872accd201ac
AnaisBonin <anaisboninfr@gmail.com>
2021-11-24 09:37:03 +0100

    cleaned default app.js file, added a Header with welcome title

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = (name) => {
+    return (
+        <header>
+            <h1>Welcome everyone! Let's play {name}!</h1>
+        </header>
+    )
+}
+
+export default Header
